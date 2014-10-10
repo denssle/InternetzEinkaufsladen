d976f244f5493109e20032066f69dbf85e77b18a
Dominik Enßle <dominik.enssle@innoq.com>
2014-10-10 10:36:57 +0200

    ArtikelVerifikation

--- src/logikabteilung/Artikelverwaltung.java
+++ src/logikabteilung/Artikelverwaltung.java
@@ -10,11 +10,37 @@ public class Artikelverwaltung
 	
 	public void artikelSpeichern(Map neuerArtikelMap)
 	{
+		kategorieVerifikation((String) neuerArtikelMap.get("kategorie"));
+		namensVerifikation((String) neuerArtikelMap.get("name"));
+		preisVerifikation((double) neuerArtikelMap.get("preis"));
+		
 		Artikel zuSpeichernderArtikel = new Artikel();
 		zuSpeichernderArtikel.setBeschreibung((String) neuerArtikelMap.get("beschreibung"));
 		zuSpeichernderArtikel.setKategorie((String) neuerArtikelMap.get("kategorie"));
 		zuSpeichernderArtikel.setName((String) neuerArtikelMap.get("name"));
-		zuSpeichernderArtikel.setPreis((double) neuerArtikelMap.get("beschreibung"));
+		zuSpeichernderArtikel.setPreis((double) neuerArtikelMap.get("preis"));
 		artikelDAO.speichern(zuSpeichernderArtikel);
+	}
+	
+	public void kategorieVerifikation(String kategorie)
+	{
+		if(kategorie.isEmpty())
+		{
+			throw new IllegalArgumentException();
+		}
+	}
+	public void namensVerifikation(String name)
+	{
+		if(name.isEmpty())
+		{
+			throw new IllegalArgumentException();
+		}
+	}
+	public void preisVerifikation(double preis)
+	{
+		if(preis <= 0)
+		{
+			throw new IllegalArgumentException();
+		}
 	}
 }
