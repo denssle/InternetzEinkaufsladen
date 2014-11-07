5e1516866c3ff41f23c29ed21df59a799a5ef455
Dominik Enßle <dominik.enssle@innoq.com>
2014-11-07 16:44:37 +0100

    Login View Update

--- src/view/LogInAnsicht.java
+++ src/view/LogInAnsicht.java
@@ -3,18 +3,44 @@ package view;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class LogInAnsicht
+public class LogInAnsicht implements ActionListener
 {	
+	private JTextField emailField = new JTextField("Email", 12);
+	private JTextField passwortField = new JTextField("Passwort", 12);
+	
 	public JPanel anmelden()
 	{
+		
 		JPanel login = new JPanel();
-		login.setLayout(new FlowLayout());
-		login.add(new TextField("Email"));
-		login.add(new TextField("Passwort"));
-		login.add(new JButton("Ok"));
+		login.setLayout(new FlowLayout());		
+		
+		emailField.setName("Email");
+		emailField.addActionListener(this);
+		
+		passwortField.setName("Passwort");
+		passwortField.addActionListener(this);
+		
+		JButton ok = new JButton("Ok");
+		ok.addActionListener(this);
+		
+		login.add(emailField);
+		login.add(passwortField);
+		login.add(ok);
 		return login;
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		if (e.getActionCommand().equals("Ok"))
+		{
+			System.out.println(emailField.getText()+" "+passwortField.getText());
+		}
 	}
 }
